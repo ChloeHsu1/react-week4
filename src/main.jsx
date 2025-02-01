6f0909ef2849fda4bfd2e25d4ed09c1096509eaa
ChloeHsu1 <chloe.hsu.2024@gmail.com>
2025-02-01 21:56:06 +0800

    完成任務

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; // 引入 CSS
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //<StrictMode>
     <App />
-  </StrictMode>,
+  //</StrictMode>,
 )
